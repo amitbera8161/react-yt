01d4e4b9970cd77a876af37628f57bfe02646a2a
Amit Bera <amitbera8161@gmail.com>
2026-04-02 18:03:24 +0530

    Added 02-components folder

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div>
+      <div className='card'>
+        <h1>Amit Bera</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, consequuntur.</p>
+      </div>
+      
+    </div>
+    </div>
+  )
+}
+
+export default Card
---
+++ 02-components/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='card'>
+        <h1>amit bera</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, consequuntur.</p>
+      </div>
+    
+    </div>
+  )
+}
+
+export default Navbar
